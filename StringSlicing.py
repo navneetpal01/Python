d410935ae63cf2ab3fdc2e1cf166b4343c4667e7
navneet <navneetpalgithub@gmail.com>
2024-06-25 10:23:31 +0530

    Marss

---
+++ StringSlicing.py
@@ -0,0 +1,9 @@
+# slicing = create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+
+name = "Bro Code"
+
+first_name = name[]
+print(first_name)
