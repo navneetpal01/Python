ae64476c7d90b47a695b1f11b5b25ab4512d2521
navneet <navneetpalgithub@gmail.com>
2024-06-25 09:37:25 +0530

    Marss

---
+++ StringMethods.py
@@ -0,0 +1,12 @@
+name = "navneet"
+
+print(len(name))
+print(name.find("N"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("e"))
+print(name.replace("e","o"))
+print(name*3)
