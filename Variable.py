5e55b34a29750c36986bfb4c2b40aadfdb34364e
navneet <navneetpalgithub@gmail.com>
2024-06-25 09:13:20 +0530

    Marss

---
+++ Variable.py
@@ -0,0 +1,49 @@
+# variable = a container for a value. Behaves as the value that it contains
+
+
+# String
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
+# print(type(name))
+
+
+# Int
+age = 21
+age += 1
+print("Your age is: " + str(age))  # converted Int to String
+print(type(age))
+
+# Float
+height = 250.5
+print("Your height is " + str(height) + "cm")
+print(type(height))
+
+# Boolean
+human = False
+print("Are you a human: " + str(human))
+print(type(human))
+
+
+
+
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
