c2e2bd00de790cda8354a467b2070955e8a5ac83
navneet <navneetpalgithub@gmail.com>
2024-06-25 10:45:55 +0530

    Marss

--- StringSlicing.py
+++ StringSlicing.py
@@ -5,5 +5,23 @@
 
 name = "Bro Code"
 
-first_name = name[]
+first_name = name[0:3]  # The first index is inclusive and the stopping index is exclusive
+# first_name = name[:3] Python will assume that the starting index is zero
+last_name = name[4:]
+# normally step is 1 by default
+funky_name = name[0:8:2]
+reversed_name = name[::-1]
+
 print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
+
+website1 = "https://google.com"
+website2 = "https://wikipedia.com"
+slice1 = slice(8, -4)
+slice2 = slice(8, -4)
+
+
+print(website1[slice1])
+print(website2[slice2])
